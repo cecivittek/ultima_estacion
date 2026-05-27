f089c7e912ef3a9aac6306a22e1ec37c8100ee3c
Ngonza02 <ngonzalezgenin@uade.edu.ar>
2026-05-27 16:15:12 -0300

    MOVIMIENTOS PP
    Ya se ajustaron los movimientos de respira, izquierda y derecha

--- Assets/PERSONAJES/PP_CONTROLADOR.cs
+++ Assets/PERSONAJES/PP_CONTROLADOR.cs
@@ -1,6 +1,23 @@
 using UnityEngine;
 
-public class PP_CONTROLADOR
+public class PP_CONTROLADOR : MonoBehaviour
 {
-    
+    private Animator animator;
+    public float velocidad = 3f;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+
+        // Mover al personaje
+        transform.Translate(new Vector2(moveX, 0) * velocidad * Time.deltaTime);
+
+        // Actualizar el Animator
+        animator.SetFloat("VelocidadX", moveX);
+    }
 }
