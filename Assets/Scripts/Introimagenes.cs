b052b1dcdab3c9de361520e13f4b492c79051e69
crespova <vcrespo@uade.edu.ar>
2026-06-13 21:43:18 -0300

    imagenes intro

---
+++ Assets/Scripts/Introimagenes.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Introimagenes : MonoBehaviour
+{
+    public Sprite[] imagenes;
+    public Image imagenPantalla;
+
+    private int indiceActual = 0;
+
+    void Start()
+    {
+        MostrarImagen(01);
+    }
+
+    public void Siguiente()
+    {
+        indiceActual++;
+
+        if (indiceActual >= imagenes.Length)
+        {
+            SceneManager.LoadScene("eleccion_objeto");
+        }
+        else
+        {
+            MostrarImagen(indiceActual);
+        }
+    }
+
+    void MostrarImagen(int indice)
+    {
+        imagenPantalla.sprite = imagenes[indice];
+    }
+}
