552b50675a72074e5d5ae62afe257da939d5f44f
Ngonza02 <ngonzalezgenin@uade.edu.ar>
2026-06-15 15:30:12 -0300

    ESCENA VICTORIA, DERROTA Y BOTON SELECCIONAR PERSONAJE ACUSACION

---
+++ Assets/Scripts/AcusacionManager.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class AcusacionManager : MonoBehaviour
+{
+    [SerializeField] private string culpable = "Milagros";
+    [SerializeField] private GameObject panelResultado;
+    [SerializeField] private TextMeshProUGUI textoResultado;
+
+    public Color colorSeleccionado = Color.green;
+    public Color colorNormal = Color.white;
+
+    private string personajeSeleccionado = "";
+    private BotonPersonaje botonActual;
+
+    public void SeleccionarPersonaje(string nombre, BotonPersonaje boton)
+    {
+        if (botonActual != null)
+            botonActual.imagenFondo.color = colorNormal;
+
+        personajeSeleccionado = nombre;
+        botonActual = boton;
+        boton.imagenFondo.color = colorSeleccionado;
+    }
+
+    public void HacerAcusacion()
+    {
+        if (personajeSeleccionado == "")
+        {
+            if (panelResultado != null)
+            {
+                textoResultado.text = "¡Seleccioná un personaje primero!";
+                panelResultado.SetActive(true);
+            }
+            return;
+        }
+
+        if (personajeSeleccionado == culpable)
+            SceneManager.LoadScene("escena_victoria");
+        else
+            SceneManager.LoadScene("escena_derrota");
+    }
+}
